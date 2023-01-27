ca797b048efb4e63847bd714436775e4bd63f4e2
Tal Ben-Horin PhD <tbenhor@ncsu.edu>
2023-01-27 15:08:06 -0500

    Oysters!

--- CFRFintsurv.R
+++ CFRFintsurv.R
@@ -2,4 +2,13 @@ rm(list=ls(all=TRUE))
 set.seed(1234)
 CFRF <- read.csv("CFRFsa.csv", fill = FALSE, header = TRUE)
 
-# head(CFRF)
+library(icenReg)
+
+BS <- CFRF[which(CFRF$site=='Nelson Bay'), ]
+
+np_fit = ic_np(cbind(t1, t2) ~ line, data = BS)
+
+
+plot(np_fit,col=c('blue','orange','red','pink'),xlab='Date',ylab = 'Survival')
+
+
