de6384d461c2144cb41bb98e538e1194857d3129
Tal Ben-Horin PhD <tbenhor@ncsu.edu>
2023-01-31 11:18:07 -0500

    Merge

--- CFRFintsurv.R
+++ CFRFintsurv.R
@@ -4,11 +4,21 @@ CFRF <- read.csv("CFRFsa.csv", fill = FALSE, header = TRUE)
 
 library(icenReg)
 
-BS <- CFRF[which(CFRF$site=='Pasture Point'), ]
+BS <- CFRF[which(CFRF$site=='Bogue Sound'), ]
+PP <- CFRF[which(CFRF$site=='Pasture Point'), ]
+WC <- CFRF[which(CFRF$site=='Williston Creek'), ]
+NB <- CFRF[which(CFRF$site=='Nelson Bay'), ]
+ST <- CFRF[which(CFRF$site=='Straits'), ]
+WM <- CFRF[which(CFRF$site=='Wilmington'), ]
 
-np_fit = ic_np(cbind(t1, t2) ~ line, data = CFRF)
+BS_fit = ic_np(cbind(t1, t2) ~ line, data = BS)
+PP_fit = ic_np(cbind(t1, t2) ~ line, data = PP)
+WC_fit = ic_np(cbind(t1, t2) ~ line, data = WC)
+NB_fit = ic_np(cbind(t1, t2) ~ line, data = NB)
+ST_fit = ic_np(cbind(t1, t2) ~ line, data = ST)
+WM_fit = ic_np(cbind(t1, t2) ~ line, data = WM)
 
+BS_curves = BS_fit$scurves
 
-plot(np_fit,col=c('blue','orange','red','pink'),xlab='Date',ylab = 'Survival',lgdLocation = "bottomleft")
-
+plot(ST_fit,col=c('blue','orange','red','pink'),xlab='Date',ylab = 'Survival',lgdLocation = "bottomleft")
 
